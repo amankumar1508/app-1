368f2929d03b237224e41a5567fb1c4f4f6f594a
amankumar <aman.kr.cg@gmail.com>
2026-04-18 01:40:00 +0530

    feat: add bulk delete notes endpoint (DELETE /api/notes/bulk)

--- src/routes/note.routes.js
+++ src/routes/note.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { createNote, bulkCreateNotes, getNotes, getNoteById, updateNote, patchNote, deleteNote } = require("../controllers/note.controller");
+const { createNote, bulkCreateNotes, getNotes, getNoteById, updateNote, patchNote, deleteNote, bulkDeleteNotes } = require("../controllers/note.controller");
 
 router.post("/", createNote);
 router.post("/bulk", bulkCreateNotes);
@@ -8,6 +8,7 @@ router.get("/", getNotes);
 router.get("/:id", getNoteById);
 router.put("/:id", updateNote);
 router.patch("/:id", patchNote);
+router.delete("/bulk", bulkDeleteNotes);
 router.delete("/:id", deleteNote);
 
 module.exports = router;

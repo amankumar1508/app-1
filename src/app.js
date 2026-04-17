73c1f8293d1fc7b6536bcbe6908ce43f59c41bc4
amankumar <aman.kr.cg@gmail.com>
2026-04-18 00:05:00 +0530

    chore: initial project setup and core MVC structure

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+
+module.exports = app;
